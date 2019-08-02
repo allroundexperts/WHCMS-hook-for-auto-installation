5add4a2f6798620e0ac1fcf53a18cd4225a48ab4
HamzaJawed <m.hamza@live.com>
2019-08-02 21:42:46 +0500

    Interfaces added

--- libraries/Interfaces/IConnect.php
+++ libraries/Interfaces/IConnect.php
@@ -1,9 +1,16 @@
 <?php  
 
+interface IConnect
+{
+	private $Username;
+	private $Password;
+	private $Port;
+	private $Url;
 
-
-
-
+	public connect();
+	public ExecuteCommand($connection, $command);
+	public TransferFile($source, $destination);
+}
 
 
 ?>

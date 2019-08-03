bf4cd7629545081b190b4b83e1c676bdeeca20c7
Hamza <m.hamza@live.com>
2019-08-03 13:42:46 +0500

    Code structure changed till fetching custom fields

--- libraries/Classes/Main.php
+++ libraries/Classes/Main.php
@@ -7,10 +7,12 @@ require_once ('/var/www/html/clients/includes/hooks/libraries/Classes/Utilities.
 
 class Main {
 	private $orderID;
+	private $helperFunctions;
 	
 	function __construct($order)
 	{
 		$this->orderID = $order;
+		$this->helperFunctions = new Utilities();
 	}
 
 	public function getCustomFields()
@@ -18,39 +20,33 @@ class Main {
 		// Using the order ID, I will fetch the details of the order.
 	    // This detail doesn't have custom field data. So i will extract client id at the moment
 
-	    $postData_getorders = array('id' => $this->orderID,);
-	    $results_getorders = localAPI('GetOrders', $postData_getorders);
-	    $results_getorders_obj = returnobj($results_getorders);
+	    $postDataGetOrders = array('id' => $this->orderID,);
+	    $resultsGetOrdersObj = $this->helperFunctions->request('GetOrders',$postDataGetOrders);
+	    $userId = $resultsGetOrdersObj->orders->order[0]->userid;
 
-	    $userid = $results_getorders_obj->orders->order[0]->userid;
+	    //Now, i have userid and orderID. The Api call that will give me custom field data is 'GetClientProducts'
+	    //In order to handle an edge case, I have to find the number of orders the client have made till date.
 
-	    // //Now, i have userid and orderID. The Api call that will give me custom field data is 'GetClientProducts'
-	    // //In order to handle an edge case, I have to find the number of orders the client have made till date.
+	    $postDataGetClientDetails = array ('clientid' => $userid, 'stats' => true,);
+	    $resultsGetClientDetailsObj = $this->helperFunctions->request('GetClientsDetails',$postDataGetClientDetails);
+	    $totalActiveOrder = $resultsGetClientDetailsObj->stats->productsnumactiveother;
 
-	    $postData_getclientdetails = array ('clientid' => $userid, 'stats' => true,);
-	    $results_getclientdetails = localAPI('GetClientsDetails', $postData_getclientdetails);
-	    $results_getclientdetails_obj = returnobj($results_getclientdetails);
-	    $total_active_order = $results_getclientdetails_obj->stats->productsnumactiveother;
+	    // Fetching custom field data
 
+	    $postDataGetClientsProduct = array ('clientid' => $userId, 'stats' => true, 'limitnum' => ($totalActiveOrder+1000),);
+	    $resultsGetClientsProductObj = $this->helperFunctions->request('GetClientsProducts',$postDataGetClientsProduct);
+	    $list_product = $resultsGetClientsProductObj->products->product;
+	    $total_result = $resultsGetClientsProductObj->totalresults;
 
-	    // //Fetching custom field data
-
-	    $postData_getclientsproduct = array ('clientid' => $userid, 'stats' => true, 'limitnum' => ($total_active_order+1000),);
-	    $results_getclientsproduct = localAPI('GetClientsProducts',$postData_getclientsproduct);
-
-	    $results_getclientsproduct_obj = returnobj($results_getclientsproduct);
-	    $list_product = $results_getclientsproduct_obj->products->product;
-	    $total_result = $results_getclientsproduct_obj->totalresults;
-
-	    $ip = returnvalue($list_product,$total_result,$this->orderID,"Server IP");
-	    $username = returnvalue($list_product,$total_result,$this->orderID,"Username");
-	    $password = returnvalue($list_product,$total_result,$this->orderID,"Password");
-	    $port = returnvalue($list_product,$total_result,$this->orderID,"SSH Port");
+	    $ip = $this->helperFunctions->returnvalue($list_product,$total_result,$this->orderID,"Server IP");
+	    $username = $this->helperFunctions->returnvalue($list_product,$total_result,$this->orderID,"Username");
+	    $password = $this->helperFunctions->returnvalue($list_product,$total_result,$this->orderID,"Password");
+	    $port = $this->helperFunctions->returnvalue($list_product,$total_result,$this->orderID,"SSH Port");
 	    
-	    logActivity($ip,0);
-	    logActivity($username,0);
-	    logActivity($password,0);
-	    logActivity($port,0);
+	    // logActivity($ip,0);
+	    // logActivity($username,0);
+	    // logActivity($password,0);
+	    // logActivity($port,0);
 
 	    return array($ip, $username, $password, $port);
 	}
--- libraries/Classes/Utilities.php
+++ libraries/Classes/Utilities.php
@@ -1,21 +1,22 @@
 <?php  
 
+class Utilities{
+    public function request($command, $postData){
+        $outputFromAPI = localAPI($command, $postData);
+        $str = json_encode($outputFromAPI);
+        $phpObj = json_decode($str);
+        return $phpObj;    
+    }
 
-//This function converts the json returend from API call to a php
-function returnobj($outputfromAPI){
-    $str = json_encode($outputfromAPI);
-    $phpobj = json_decode($str);
-    return $phpobj;
-}
-
-function returnvalue ($array_product, $totalproduct, $orderID, $valuetofind){
-    for ($x = 0; $x <= $totalproduct; $x++){
-        if ($array_product[$x]->id == $orderID){
-            if ($array_product[$x]->pid == 1){
-                $field = $array_product[$x]->customfields->customfield;
-                for ($y = 0; $y <= 4; $y++){
-                    if ($field[$y]->name == $valuetofind){
-                        return $field[$y]->value;
+    public function returnvalue($array_product, $totalproduct, $orderID, $valuetofind){
+        for ($x = 0; $x <= $totalproduct; $x++){
+            if ($array_product[$x]->id == $orderID){
+                if ($array_product[$x]->pid == 1){
+                    $field = $array_product[$x]->customfields->customfield;
+                    for ($y = 0; $y <= 4; $y++){
+                        if ($field[$y]->name == $valuetofind){
+                            return $field[$y]->value;
+                        }
                     }
                 }
             }
@@ -23,20 +24,54 @@ function returnvalue ($array_product, $totalproduct, $orderID, $valuetofind){
     }
 }
 
-function connect ($ip, $username, $password, $port){
-    $conn = ssh2_connect($ip,$port);
-    ssh2_auth_password($conn, $username, $password);
-    return $conn;
-}
 
-function execute_command($conn,$command){
 
-    $stream = ssh2_exec($conn, $command);
-    stream_set_blocking($stream, true);
-    $stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
-    $output = stream_get_contents($stream_out);
-    return $output;
-}
+
+
+
+
+
+
+
+
+
+
+// //This function converts the json returend from API call to a php
+// function returnobj($outputfromAPI){
+//     $str = json_encode($outputfromAPI);
+//     $phpobj = json_decode($str);
+//     return $phpobj;
+// }
+
+// function returnvalue ($array_product, $totalproduct, $orderID, $valuetofind){
+//     for ($x = 0; $x <= $totalproduct; $x++){
+//         if ($array_product[$x]->id == $orderID){
+//             if ($array_product[$x]->pid == 1){
+//                 $field = $array_product[$x]->customfields->customfield;
+//                 for ($y = 0; $y <= 4; $y++){
+//                     if ($field[$y]->name == $valuetofind){
+//                         return $field[$y]->value;
+//                     }
+//                 }
+//             }
+//         }
+//     }
+// }
+
+// function connect ($ip, $username, $password, $port){
+//     $conn = ssh2_connect($ip,$port);
+//     ssh2_auth_password($conn, $username, $password);
+//     return $conn;
+// }
+
+// function execute_command($conn,$command){
+
+//     $stream = ssh2_exec($conn, $command);
+//     stream_set_blocking($stream, true);
+//     $stream_out = ssh2_fetch_stream($stream, SSH2_STREAM_STDIO);
+//     $output = stream_get_contents($stream_out);
+//     return $output;
+// }
 
 
 
